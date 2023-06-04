6a37d881977e08820dc04824f9daddce635fc986
Константин Харьков <stenkh@yandex.ru>
2023-06-05 00:35:59 +0700

    updated db models

--- app/models.py
+++ app/models.py
@@ -5,26 +5,33 @@ from .database import Base
 from sqlalchemy.orm import mapped_column
 
 class User(Base):
-    username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
-    job_title = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
+    username = mapped_column(String, nullable=False)
+    password = mapped_column(String, nullable=False)
+    name = mapped_column(String, nullable=False)
+    surname = mapped_column(String, nullable=False)
+    job_title = mapped_column(String, nullable=False)
+    is_active = mapped_column(Boolean, default=True)
+    is_superuser = mapped_column(Boolean, default=False)
     salary = relationship('Salary', back_populates='employee')
 
 
 class Salary(Base):
-    salary = Column(Integer, nullable=False)
-    employee_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    next_increase_date = Column(DateTime, nullable=False)
+    salary = mapped_column(Integer, nullable=False)
+    employee_id = mapped_column(
+        Integer,
+        ForeignKey('user.id'),
+        nullable=False,
+        unique=True, 
+        sqlite_on_conflict_unique='REPLACE'
+    )
+    next_increase_date = mapped_column(DateTime, nullable=False)
     employee = relationship("User", back_populates='salary')
+    '''
     __table_args__ = (
         UniqueConstraint(
             'employee_id', 
             name='uc_salary_employee_id',
             sqlite_on_conflict='REPLACE'),
     )
-
+    '''
 
