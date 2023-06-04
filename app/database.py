5bde8b6dddd506a3fdcb9dfd397085edb9352ec7
Константин Харьков <stenkh@yandex.ru>
2023-06-04 23:44:58 +0700

    added mapped_column import

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
 
 from .config import settings
-
+from sqlalchemy.orm import mapped_column
 
 class PreBase:
 
--- app/models.py
+++ app/models.py
@@ -2,26 +2,29 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, U
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from .database import Base
-
+from sqlalchemy.orm import mapped_column
 
 class User(Base):
-    username = Column(String)
-    password = Column(String)
-    name = Column(String)
-    surname = Column(String)
-    job_title = Column(String)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    job_title = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
     salary = relationship('Salary', back_populates='employee')
 
 
 class Salary(Base):
-    salary = Column(Integer)
-    employee_id = Column(Integer, ForeignKey('user.id'))
-    next_increase_date = Column(DateTime)
+    salary = Column(Integer, nullable=False)
+    employee_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    next_increase_date = Column(DateTime, nullable=False)
     employee = relationship("User", back_populates='salary')
     __table_args__ = (
-        UniqueConstraint('employee_id', name='uc_salary_employee_id, '),
+        UniqueConstraint(
+            'employee_id', 
+            name='uc_salary_employee_id',
+            sqlite_on_conflict='REPLACE'),
     )
 
 
